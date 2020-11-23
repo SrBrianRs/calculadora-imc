711806fc7be3c5a7564a6535e064c586f800f027
Yorielmonje <Yoriel.monje.espidio.cet146@gmail.com>
2020-11-23 13:55:44 -0600

    formulario sin estilo #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CalculadoraUiComponent } from './calculadora-ui/calculadora-ui.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CalculadoraUiComponent } from './calculadora-ui/calculadora-ui.componen
     CalculadoraUiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculadora-ui/calculadora-ui.component.ts
+++ src/app/calculadora-ui/calculadora-ui.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import calcularHombre from '../calculadora-hombre/hombre';
+import calcularMujer from '../calculadora-mujer/mujer';
 
 @Component({
   selector: 'app-calculadora-ui',
@@ -9,7 +11,35 @@ export class CalculadoraUiComponent implements OnInit {
 
   constructor() { }
 
+  imc = null;
+  diagnostico = null;
+  edad = null;
+  sexo = null;
+  peso = null;
+  altura = null;
+
+
   ngOnInit(): void {
   }
 
+  calcularImc(){
+    if(this.peso !== null && this.altura !== null){
+
+      if(this.sexo === 'hombre'){
+        this.imc = calcularHombre(this.peso,this.altura);
+      } else {
+        this.imc = calcularMujer(this.peso, this.altura);
+      }
+
+      if( this.imc < 18.5){
+        this.diagnostico = "Peso Bajo";
+      } else if (this.imc >= 18.5 && this.imc <= 24.9) {
+        this.diagnostico = "Peso Normal";
+      } else if (this.imc >= 25.0 && this.imc <= 29.9){
+        this.diagnostico = "Sobrepeso";
+      } else {
+        this.diagnostico = "Obesidad";
+      }
+    }
+  }
 }
