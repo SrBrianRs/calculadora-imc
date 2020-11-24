20de8984ded9b1a1228d76a6f144848934fe071a
Yorielmonje <Yoriel.monje.espidio.cet146@gmail.com>
2020-11-24 13:50:13 -0600

    diseño añadido #3

--- src/app/calculadora-ui/calculadora-ui.component.ts
+++ src/app/calculadora-ui/calculadora-ui.component.ts
@@ -23,7 +23,10 @@ export class CalculadoraUiComponent implements OnInit {
   }
 
   calcularImc(){
-    if(this.peso !== null && this.altura !== null){
+
+    if(this.peso !== null && this.altura !== null && this.sexo !== null && this.edad !== null){
+
+      borraFlecha();
 
       if(this.sexo === 'hombre'){
         this.imc = calcularHombre(this.peso,this.altura);
@@ -33,13 +36,24 @@ export class CalculadoraUiComponent implements OnInit {
 
       if( this.imc < 18.5){
         this.diagnostico = "Peso Bajo";
+        document.querySelector('#f1').innerHTML = '&#8681'
       } else if (this.imc >= 18.5 && this.imc <= 24.9) {
         this.diagnostico = "Peso Normal";
+        document.querySelector('#f2').innerHTML = '&#8681'
       } else if (this.imc >= 25.0 && this.imc <= 29.9){
         this.diagnostico = "Sobrepeso";
+        document.querySelector('#f3').innerHTML = '&#8681'
       } else {
         this.diagnostico = "Obesidad";
+        document.querySelector('#f4').innerHTML = '&#8681'
       }
+      
     }
   }
+}
+function borraFlecha(){
+  document.querySelector('#f1').innerHTML = '';
+  document.querySelector('#f2').innerHTML = '';
+  document.querySelector('#f3').innerHTML = '';
+  document.querySelector('#f4').innerHTML = '';
 }
